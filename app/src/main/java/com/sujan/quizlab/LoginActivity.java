ef19724017d37a65cc4b65eb21155162ece9cf03
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2022-12-26 17:03:10 +0600

    sign in layout done

--- app/src/main/java/com/sujan/quizlab/LoginActivity.java
+++ app/src/main/java/com/sujan/quizlab/LoginActivity.java
@@ -3,12 +3,30 @@ package com.sujan.quizlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+    }
+
+
+    public void handleSignClick(View v) {
+        Button _button = (Button) v;
+
+    }
+
+    public void handleChangeSignOption(View v) {
+        View _mV = findViewById(R.id.buttonSign);
+        Log.d("Message", "someText");
     }
 }
--- app/src/main/java/com/sujan/quizlab/SplashActivity.java
+++ app/src/main/java/com/sujan/quizlab/SplashActivity.java
@@ -7,11 +7,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
-import com.sujan.quizlab.ui.login.LoginActivity;
+
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
